ffd55882799df003e670bca5a43078ef18c2f7f1
troyyxk <xingkunyin@hotmail.com>
2020-12-11 00:25:25 -0500

    Update

--- devflow/src/services/authService.js
+++ devflow/src/services/authService.js
@@ -19,7 +19,7 @@ export const checkSession = (app) => {
 
 export const login = async (userName, password) => {
   // Create our request constructor with all the parameters we need
-
+  const url = process.env.NODE_ENV === 'production' ? host + "/auth/login" : host + port + "/auth/login"
   const request = new Request(host + port + "/auth/login", {
     method: "post",
     body: JSON.stringify({
