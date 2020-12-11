c4945473a423387b83978014c113b7ffa561b2a7
troyyxk <xingkunyin@hotmail.com>
2020-12-11 00:36:44 -0500

    Update

--- devflow/src/config.js
+++ devflow/src/config.js
@@ -1,6 +1,6 @@
 export const config = {
     backend: {
-        host: process.env.NODE_ENV === 'production' ? "/" : "http://localhost:",
+        host: process.env.NODE_ENV === 'production' ? "https://devflowproject.herokuapp.com" : "http://localhost:",
         port: process.env.NODE_ENV === 'production' ? process.env.PORT : 5000
     }
 };
